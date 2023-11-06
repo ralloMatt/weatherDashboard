1be824f59c2a0ae96c8e835dbc95202d3a13e495
Matt R <mattrallo2012@gmail.com>
2023-11-06 00:31:41 -0600

    todays forecast displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,85 @@
 var searchForm = document.getElementById("searchForm"); // the form for searches
 var userInput = document.getElementById("formInput"); // get the input user typed
 var buttonList = document.getElementById("searchHistory"); // get the container for the search history
+var infoText = document.getElementById("infoText");
+var todayWeather = document.getElementById("todayWeather");
 
 var APIKEY = "b3712b4761003710bde48a0606fe6822"; //API key for Open Weather Map
 
-function getWeather(lat, lon){
+function displayTodaysWeather(cityWeather){ // display the weather from said city
+    infoText.remove(); // remove information text
 
+    // remove any information that may be there (so I don't display multiple entries)
+    if(document.getElementById("todaysInfo")){ 
+        document.getElementById("todaysInfo").remove();
+    }
+
+    // create elements and get information
+    var todayInformation = document.createElement("section"); // created section so I can remove later
+    todayInformation.id = "todaysInfo";
+    var cityNamePlusDate = document.createElement("h2"); // city name and date
+    var today = dayjs();
+    var weekDay = dayjs(today, "M-D-YYYY").format("dddd"); // get the day of the week
+    cityNamePlusDate.textContent = cityWeather.name + " (" + weekDay + " " + today.format("MMMM DD, YYYY" + ")"); // set name
+    cityNamePlusDate.style.display = "inline"; // prevent icon from going to next line
+
+    todayInformation.append(cityNamePlusDate);
+
+    // get icon
+    var iconNum = cityWeather.weather[0].icon;
+    var iconUrl = "https://openweathermap.org/img/wn/" + iconNum + "@2x.png";
+    var iconImg = document.createElement("img");
+    iconImg.setAttribute("src", iconUrl);
+    todayInformation.append(iconImg);
+
+    var temp = document.createElement("h3");
+    temp.textContent = "Temp: " + cityWeather.main.temp + " \u00B0F";
+    todayInformation.append(temp);
+
+    var wind = document.createElement("h3");
+    wind.textContent = "Wind: " + cityWeather.wind.speed + " MPH";
+    todayInformation.append(wind);
+
+    var humidity = document.createElement("h3");
+    humidity.textContent = "Humidity: " + cityWeather.main.humidity + " %";
+    todayInformation.append(humidity);
+
+    todayWeather.append(todayInformation);
+}
+
+function getWeather(lat, lon){ // Get weather data from API
     // Now that we have Coordinates, find the weather dude!
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY;
 
-    fetch(weatherUrl) // send out request and fetch should return an object
+    var weatherUrlToday = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY + "&units=imperial";
+
+    // get todays weather
+    fetch(weatherUrlToday) // send out request and fetch should return an object
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {  
-        console.log(data);
+        displayTodaysWeather(data);
     });
-    
-    
+
+
+    /*
+    // get the five day forecast
+    var weatherUrlFiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKEY + "&units=imperial";
+
+    fetch(weatherUrlFiveDay) // send out request and fetch should return an object
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {  
+        displayFiveDayForecast(data);
+    });
+    */
+
 }
 
-function findCity(city){
+function findCity(city){ // get city data from API
     // creat URL using city name and API key
-    var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city +" &limit=1&appid=" + APIKEY;
+    var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&appid=" + APIKEY;
     
     //First get coordinates
     fetch(geoCodeUrl) // send out request and fetch should return an object
